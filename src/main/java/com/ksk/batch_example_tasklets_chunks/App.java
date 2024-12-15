fb6ed10a46a6fa2fd5130d784868cb3c5a65df0e
kharakkular <kharakkular@gmail.com>
2024-12-15 01:43:34 -0500

    initializzed spring batch tasklet and chunks example project

---
+++ src/main/java/com/ksk/batch_example_tasklets_chunks/App.java
@@ -0,0 +1,10 @@
+package com.ksk.batch_example_tasklets_chunks;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
