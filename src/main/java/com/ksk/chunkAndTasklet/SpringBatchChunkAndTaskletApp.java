4591e6e6d79223c8263887a91c57c1c1e9584f10
kharakkular <kharakkular@gmail.com>
2024-12-16 00:25:52 -0500

    added code for fileutils class and the main runner class

--- src/main/java/com/ksk/chunkAndTasklet/SpringBatchChunkAndTaskletApp.java
+++ src/main/java/com/ksk/chunkAndTasklet/SpringBatchChunkAndTaskletApp.java
@@ -1,10 +1,44 @@
 package com.ksk.chunkAndTasklet;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.ksk.chunkAndTasklet.config.TaskletConfig;
+
 public class SpringBatchChunkAndTaskletApp {
-
+	private static Logger logger = LogManager.getLogger(SpringBatchChunkAndTaskletApp.class);
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+		context.register(TaskletConfig.class);
+		context.refresh();
+		
+		JobLauncher launcher = (JobLauncher) context.getBean("jobLauncher");
+		Job job = (Job) context.getBean("taskletJob");
+		
+		JobParameters jobParameters = new JobParametersBuilder().addString("jobId", String.valueOf(System.currentTimeMillis())).toJobParameters();
+		
 
+		try {
+			JobParameters jobparameters = new JobParametersBuilder().addString("jobId", String.valueOf(System.currentTimeMillis()))
+												.toJobParameters();
+			final JobExecution execution = launcher.run(job, jobparameters);
+			logger.info("Job Status: {}", execution.getStatus());
+		} catch( final Exception ex) {
+			ex.printStackTrace();
+			logger.error("Job failed {}", ex.getMessage());
+		}
+		
 	}
 
 }
--- src/main/java/com/ksk/chunkAndTasklet/utils/FileUtils.java
+++ src/main/java/com/ksk/chunkAndTasklet/utils/FileUtils.java
@@ -1,5 +1,93 @@
 package com.ksk.chunkAndTasklet.utils;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.ksk.chunkAndTasklet.model.Line;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+
 public class FileUtils {
+	Logger logger = LogManager.getLogger(FileUtils.class);
 	
+	private String fileName;
+
+	private FileReader fileReader;
+	private CSVReader csvReader;
+
+	private FileWriter fileWriter;
+	private CSVWriter csvWriter;
+
+	private File file;
+
+	public FileUtils(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public Line readLine(){
+		try {
+			if (fileReader == null) initReader();
+			String[] line = csvReader.readNext();
+			if (line == null)
+				return null;
+			Line tempLine = new Line();
+			tempLine.setName(line[0]);
+			tempLine.setDob(LocalDate.parse(line[1], DateTimeFormatter.ofPattern("MM/dd/yyyy")));
+			return tempLine;
+//				Line line = new Line(csvReader., null, null)
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public void writeLine(Line line) throws IOException {
+		if(fileWriter == null) initWriter();
+		
+		csvWriter.writeNext(new String[] { line.getName(), line.getAge().toString()});
+	}
+
+	private void initReader() throws FileNotFoundException {
+		
+		ClassLoader classLoader = this.getClass().getClassLoader();
+		if (file == null) file = new File(classLoader.getResource(fileName).getFile());
+		logger.info("Does the file exists: {}", file.exists());
+		
+//			file = new File(classLoader.getResource(fileName).getFile());
+		if (fileReader == null) fileReader = new FileReader(file);
+
+		if (csvReader == null) csvReader = new CSVReader(fileReader);
+	}
+
+	private void initWriter() throws IOException {
+		if (file == null) {
+			file = new File(fileName);
+			file.createNewFile();
+		}
+
+		if (fileWriter == null)
+			fileWriter = new FileWriter(file);
+		if (csvWriter == null)
+			csvWriter = new CSVWriter(fileWriter);
+	}
+
+	public void closeReader() throws IOException {
+		fileReader.close();
+		csvReader.close();
+	}
+
+	public void closeWriter() throws IOException {
+		fileWriter.close();
+		csvWriter.close();
+	}
 }
